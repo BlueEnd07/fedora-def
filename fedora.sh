14de55850b0d8ef6e21efb5745588f0383326908
BlueEnd07 <74140685+BlueEnd07@users.noreply.github.com>
2023-07-16 12:25:02 +0530

    Update fedora.sh
    make new addition in script like 
    adding speed boost
    openH264
    battry life for laptops

--- fedora.sh
+++ fedora.sh
@@ -22,8 +22,11 @@ sudo dnf update -y
 #enable rpmfusion
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-#enabling flatpack 
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+#Battery life
+sudo dnf install tlp tlp-rdw
+sudo systemctl mask power-profiles-daemon
+sudo dnf install powertop
+sudo powertop --auto-tune
 
 #Install Media Codecs
 sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
@@ -32,18 +35,35 @@ sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=Pack
 sudo dnf install akmod-nvidia -y # rhel/centos users can use kmod-nvidia instead 
 sudo dnf install xorg-x11-drv-nvidia-cuda -y #optional for cuda/nvdec/nvenc support
 
+sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+sudo dnf groupupdate sound-and-video -y
+sudo dnf install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel ffmpeg gstreamer-ffmpeg
+sudo dnf install lame\* --exclude=lame-devel -y
+sudo dnf group upgrade --with-optional Multimedia -y
+sudo dnf install ffmpeg ffmpeg-libs libva libva-utils
 
+#OpenH264 for Firefox
+sudo dnf config-manager --set-enabled fedora-cisco-openh264
+sudo dnf install -y gstreamer1-plugin-openh264 mozilla-openh264
+
+#update flatpack
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak update
 #install app
-
 sudo dnf install blender vlc gnome-tweak-tool chrome-gnome-shell gnome-extensions-app kdeconnectd steam -y
-flatpak install flathub net.lutris.Lutris -y
-flatpak install flathub com.discordapp.Discord -y
+flatpak install flathub net.lutris.Lutris -yflatpak install flathub com.discordapp.Discord -y
 flatpak install flathub com.github.tchx84.Flatseal -y
-flatpak install flathub org.qbittorrent.qBittorrent -y
 flatpak install flathub md.obsidian.Obsidian -y
 flatpak install flathub com.brave.Browser -y
 
-#Better Fonts:
-sudo dnf copr enable dawid/better_fonts -y
-sudo dnf install fontconfig-font-replacements -y
-sudo dnf install fontconfig-enhanced-defaults -y
+
+#Speed Boost
+sudo dnf install grub-customizer -y
+
+#gnome Extenstions
+#for Gsconnect
+sudo dnf install nautilus-python
+
+
+
+ 
